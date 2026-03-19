ad4cdd900bd49e9e7be3a14f397bed40df49d443
IsraelCastro85 <166553771+IsraelCastro85@users.noreply.github.com>
2026-03-19 12:02:12 -0500

    Delete src directory

--- src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { handleLogin, handleMe } = require('../controllers/authController');
-const { requireAuth } = require('../middleware/auth.middleware');
-
-const router = express.Router();
-router.post('/login', handleLogin);
-router.get('/me',     requireAuth, handleMe);
-
-module.exports = router;
